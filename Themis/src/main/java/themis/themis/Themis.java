a425f84261b140819538cf4ff685eaf9fde97301
Matheus Soares <matheus.soares@prismainformatica.com.br>
2023-03-23 13:50:50 -0300

    Criado projeto java para as aulas de Progrmação Orientada a Objetos

---
+++ Themis/src/main/java/themis/themis/Themis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package themis.themis;
+
+/**
+ *
+ * @author Diego
+ */
+public class Themis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
