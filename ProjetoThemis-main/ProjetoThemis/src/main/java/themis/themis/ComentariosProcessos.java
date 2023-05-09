182a7d0bdf2997fb3a7d50630f7f7344dafbcaad
Nathan Graebin <Nathan Graebin@DESKTOP-I15APL5>
2023-05-09 20:49:36 -0300

    Refatoração Menu 1.0

--- ProjetoThemis-main/ProjetoThemis/src/main/java/themis/themis/ComentariosProcessos.java
+++ ProjetoThemis-main/ProjetoThemis/src/main/java/themis/themis/ComentariosProcessos.java
@@ -3,12 +3,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package themis.themis;
+
 import java.util.*;
+
 /**
  *
  * @author Renan Alves Peregrino Desenvolvedor, Felipe Koji Analista
+ * Desenvolvedor Atual: Nathan Felipe Oestreich Graebin
  */
 public class ComentariosProcessos {
+
     private int numAudiencia;
     private int numProcesso;
     public String dataAudiencia;
@@ -19,43 +23,69 @@ public class ComentariosProcessos {
     public String comentarios;
     public String dataComentario;
     private String nomeEmpregador;
- 
 
-    public void comentariosProcesso(int anumProcesso, int anumAudiencia, String adataAudiencia, String otribunal, String avara, String oassunto, String oscomentarios, String adataComentario, String onomeEmpregador){
-         Scanner coment = new Scanner(System.in);
-            this.numProcesso = anumProcesso;
-            this.numAudiencia = anumAudiencia;
-            this.dataAudiencia = adataAudiencia;
-            this.tribunal = otribunal;
-            this.vara = avara;
-            this.assunto = oassunto;
-            this.comentarios = oscomentarios;
-            this.dataComentario = adataComentario;
-            this.nomeEmpregador = onomeEmpregador;
-                
-        
-         
-         System.out.println("O Processo:"+numProcesso);
-         System.out.println("Com a Audiência"+numAudiencia);
-         System.out.println("Com a data para : "+dataAudiencia);
-         System.out.println("Do tribunal "+tribunal);
-         System.out.println("Da Vara:"+vara);
-         System.out.println("Do Empregador"+nomeEmpregador);
-         System.out.println("A data deste comentário é: "+dataComentario);
-         
-       
-                                
-                                
+    public void comentariosProcesso(int pNumProcesso, int pNumAudiencia, String pDataAudiencia, String pTribunal, String pVara, String pAssunto, String pComentarios, String pDataComentario, String pNomeEmpregador) {
+        Scanner coment = new Scanner(System.in);
+        this.numProcesso = pNumProcesso;
+        this.numAudiencia = pNumAudiencia;
+        this.dataAudiencia = pDataAudiencia;
+        this.tribunal = pTribunal;
+        this.vara = pVara;
+        this.assunto = pAssunto;
+        this.comentarios = pComentarios;
+        this.dataComentario = pDataComentario;
+        this.nomeEmpregador = pNomeEmpregador;
+
+        System.out.println("O Processo:" + numProcesso);
+        System.out.println("Com a Audiência" + numAudiencia);
+        System.out.println("Com a data para : " + dataAudiencia);
+        System.out.println("Do tribunal " + tribunal);
+        System.out.println("Da Vara:" + vara);
+        System.out.println("Do Empregador" + nomeEmpregador);
+        System.out.println("A data deste comentário é: " + dataComentario);
+
     }
 
-    public void indentificarComentario(int numProcesso, int numAudiencia, String dataAudiencia, String tribunal, String vara, String assunto, String comentarios, String dataComentario, String nomeEmpregador) {
-        System.out.println("O comentário Feito foi:"+this.comentarios);
+    //Metodos
+    public void indentificarComentario(int pNumProcesso, int pNumAudiencia, String pDataAudiencia, String pTribunal, String pVara, String pAssunto, String pComentarios, String pDataComentario, String pNomeEmpregador) {
+        System.out.println("O comentário Feito foi:" + this.comentarios);
     }
 
-    public void apagarComentario(String comentarios, String dataComentario){
+    public void apagarComentario(String aComentarios, String aDataComentario) {
         System.out.println("O comentário foi apagado com sucesso!");
     }
-    
+
+    //Getters And Seters
+    public int getNumAudiencia() {
+        return numAudiencia;
     }
 
+    public void setNumAudiencia(int numAudiencia) {
+        this.numAudiencia = numAudiencia;
+    }
 
+    public int getNumProcesso() {
+        return numProcesso;
+    }
+
+    public void setNumProcesso(int numProcesso) {
+        this.numProcesso = numProcesso;
+    }
+
+    public String getAssunto() {
+        return assunto;
+    }
+
+    public void setAssunto(String assunto) {
+        this.assunto = assunto;
+    }
+
+    public String getNomeEmpregador() {
+        return nomeEmpregador;
+    }
+
+    public void setNomeEmpregador(String nomeEmpregador) {
+        this.nomeEmpregador = nomeEmpregador;
+    }
+
+}
