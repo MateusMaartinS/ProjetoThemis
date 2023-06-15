2475139dc043f374c96d9a66a3e87a2bb25d1df4
Nathan Graebin <Nathan Graebin@DESKTOP-I15APL5>
2023-06-14 22:01:03 -0300

    Att 2.5 Formatação

--- ProjetoThemis-main/ProjetoThemis/src/main/java/themis/themis/Themis.java
+++ ProjetoThemis-main/ProjetoThemis/src/main/java/themis/themis/Themis.java
@@ -16,31 +16,31 @@ public class Themis {
                 "", "", "", "", "", "");
             
 
-            System.out.println("Bem vindo! Escolha uma das opções!");
+            System.out.println("BEM VINDO! ESCOLHA UMA DAS OPÇÕES!");
             System.out.println("-------------------------------------------------");
-            System.out.println("1 - Cadastrar um Processo Civil.");
-            System.out.println("2 - Acessar Resumo de um processo");
-            System.out.println("3 - Agendar uma Audiência.");
-            System.out.println("4 - Inserir comentário em um processo");
-            System.out.println("5 - Adicionar um Lembrete.");
-            System.out.println("6 - Finalizar um Processo.");
-            System.out.println("7 - Sair");
+            System.out.println("1 - CADASTRAR UM PROCESSO CIVIL.");
+            System.out.println("2 - ACESSAR RESUMO DE UM PROCESSO");
+            System.out.println("3 - AGENDAR UMA AUDIÊNCIA");
+            System.out.println("4 - INSERIR COMENTÁRIO EM UM PROCESSO");
+            System.out.println("5 - ADICIONAR UM LEMBRETE");
+            System.out.println("6 - FINALIZAR UM PROCESSO");
+            System.out.println("7 - SAIR");
             System.out.println("-------------------------------------------------");
             int opcao = Integer.parseInt(teclado.nextLine());
 
             switch (opcao) {
                 case 1:
-                    System.out.println("Você escolheu Cadastrar um Processo Civil.");
+                    System.out.println("VOCÊ ESCOLHEU CADASTRAR UM PROCESSO CIVIL");
                     cadastrar = new CadastroProcesso(1, "Assunto", "Mateus", "João", "OK", "03/03/2023", "Tribunal", "Vara", "Quebra de Contrato", "Governo");
                     cadastrar.verificarDados();
                     break;
                 case 2:
-                    System.out.println("Você escolheu Acessar Resumo.");
+                    System.out.println("VOCÊ ESCOLHEU ACESSAR O RESUMO");
                     ResumoProcesso resumir = new ResumoProcesso(1, "Data cadastro: ", "Data audiencia: ", "Tribunal: ", "Vara: ", "Categoria: ", "Comentarios: ", "Nome empregador: ", "Nome advogado: ", "Orgão judicial: ");
                     resumir.atualizarDados(1);
                     break;
                 case 3:
-                    System.out.println("Você escolheu Agendar uma Audiência.");
+                    System.out.println("VOCÊ ESCOLHEU AGENDAR UMA AUDIÊNCIA.");
                     agendar = new AgendarAudiencia(1, 2, "03/09/2002", "tribunal", "vara", "orgao", "Victor", "Matheus", "ativo");
                     //agendar.verificarAudiencia(1);
                     break;
@@ -52,17 +52,17 @@ public class Themis {
                     
                     
                 case 5:
-                    System.out.println("Você escolheu Adicionar um Lembrete.");
-                    System.out.println("1 - Adicionar Lembrete");
-                    System.out.println("2 - Mostrar Bilhete");
-                    System.out.println("3 - Sair");
+                    System.out.println("VOCÊ ESCOLHEU ADICIONAR UM LEMBRETE");
+                    System.out.println("1 - ADICIONAR LEMBRETE");
+                    System.out.println("2 - MOSTRAR BILHETE");
+                    System.out.println("3 - SAIR");
                     int innerOpcao = Integer.parseInt(teclado.nextLine());
                     LembreteProcesso lembreteProcesso = new LembreteProcesso(1, 1, 01, new Date().toString(),
                             "Tribunal", "Vara", "Empregador", "Advogado", "Categoria", "Assunto", "Em Amdamento");
                     String bilhete;
                     switch (innerOpcao) {
                         case 1:
-                            System.out.println("Escreva o lembrete que deseja adicionar: ");
+                            System.out.println("ESCREVER O LEMBRETE QUE DESEJA ADICIONAR: ");
                             bilhete = teclado.nextLine();
                             lembreteProcesso.gerarLembrete(1, new Date().toString(), "Tribunal", bilhete, "Status");
                             break;
@@ -70,7 +70,7 @@ public class Themis {
                             lembreteProcesso.mostrarLembrete(01);
                             break;
                         case 3:
-                            System.out.println("Você escolheu Sair!");
+                            System.out.println("VOCÊ ESCOLHEU SAIR!");
                             break;
                     }
                     break;
@@ -78,11 +78,11 @@ public class Themis {
                     finalizarProcesso.menuFinalizarProcesso();
                     break;
                 case 7:
-                    System.out.println("Você escolheu Sair");
+                    System.out.println("VOCÊ ESCOLHEU SAIR");
                     i = 1;
                     break;
                 default:
-                    System.out.println("Opção Invalida!");
+                    System.out.println("OPÇÃO INVALIDA!");
             }
         }
     }
